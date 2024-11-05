9eccb983c75b88a10ea7dbcdd7d447486dd7038f
Akhil092004 <akhiljain092004@gmail.com>
2024-11-05 19:32:28 +0530

    added socket.io

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -1,14 +1,31 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import { Server } from "socket.io";
+import {createServer} from "http"
+import requestIp from "request-ip";
 
 const app = express();
 
+const httpServer = createServer(app)
 
-app.use(cors({
-    origin: process.env.CORD_ORIGIN,
-    credentials:true
-}))
+const io = new Server(httpServer,{
+    cors: {
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    },
+})
+
+app.set("io",io)
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN === "*",
+        credentials : true
+    })
+)
+
+app.use(requestIp.mw())
 
 //some middle ware config
 app.use(express.json({limit : "16kb"}))
@@ -24,4 +41,4 @@ import userRouter from "../src/routes/user.routes.js"
 app.use("/api/v1/users",userRouter)
 
 
-export {app}
+export {httpServer}
